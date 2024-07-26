643b68f8ee911dd7099fb6c6a8d6765957df8fce
Yibin Long <yibinlong@outlook.com>
2024-07-26 14:00:28 -0400

    update knex schema

--- database/migrations/20240725024123_create_users_table.js
+++ database/migrations/20240725024123_create_users_table.js
@@ -1,20 +1,14 @@
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
+
 exports.up = function(knex) {
     return knex.schema.createTable('users', function(table) {
       table.increments('id').primary(); // Auto-incrementing ID column
-      table.string('name');             // Name column
-      table.string('email').unique();   // Email column with unique constraint
-      table.timestamps();               // Created_at and updated_at columns
+      table.string('username').notNullable();             // Name column
+      table.string('email').notNullable().unique();   // Email column with unique constraint
+      table.string('password_hash').notNullable(); // Password
+      table.timestamps(true, true);               // Created_at and updated_at columns
     });
 };
 
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
 exports.down = function(knex) {
     return knex.schema.dropTable('users');
 };
