bf54362c4143d4d813d57c1ab732f457642223f5
Yibin Long <yibinlong@outlook.com>
2024-08-09 17:33:59 -0400

    update server js for authRoutes

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 // server.js
 const express = require('express');
 const app = express();
+const authRoutes = require('./routes/auth');
+
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use('/api/auth', authRoutes);
 
 app.get('/', (req, res) => {
   res.send('Amazon Repricer Backend');
