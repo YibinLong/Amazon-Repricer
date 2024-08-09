744ccb2aab5e7d29c5064ae10e930bc41ae2b0e9
Yibin Long <yibinlong@outlook.com>
2024-08-09 17:46:29 -0400

    update server.js to use protectedRoutes

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,13 @@
 const express = require('express');
 const app = express();
 const authRoutes = require('./routes/auth');
+const protectedRoutes = require('./routes/protectedRoutes');
 
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use('/api/auth', authRoutes);
+app.use('/api', protectedRoutes); 
 
 app.get('/', (req, res) => {
   res.send('Amazon Repricer Backend');
