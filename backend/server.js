2801b263fe80ceb043032a07acd0d6c4813d37db
Yibin Long <yibinlong@outlook.com>
2024-08-13 22:35:29 -0400

    update server.js for new order route

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,18 @@
 // server.js
 const express = require('express');
-const app = express();
+
+
 const authRoutes = require('./routes/auth');
 const protectedRoutes = require('./routes/protectedRoutes');
+const amazonRoutes = require('./routes/amazon');
 
+const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api', protectedRoutes); 
+app.use('/api/amazon', amazonRoutes)
 
 app.get('/', (req, res) => {
   res.send('Amazon Repricer Backend');
