f8e4141f1969189e8c53c53710272b58f3c3a8d6
Yibin Long <yibinlong@outlook.com>
2024-09-21 15:32:17 -0700

    add imports to App.js

--- frontend/amazon-repricer-frontend/src/App.js
+++ frontend/amazon-repricer-frontend/src/App.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Register from './pages/Register';
+import Login from './pages/Login';
 
 function App() {
   return (
