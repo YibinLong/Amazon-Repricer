bc2189fdc55af4768d9fb87b78b757a6d3fb87ce
Yibin Long <yibinlong@outlook.com>
2024-09-21 16:04:31 -0700

    setup PricingRulesForm component

---
+++ frontend/amazon-repricer-frontend/src/components/PricingRulesForm.js
@@ -0,0 +1,9 @@
+import React, { useState } from 'react'
+
+const PricingRulesForm = ({ productID }) => {
+    const [minPrice, setMinPrice] = useState('')
+    const [maxPrice, setMaxPrice] = useState('')
+
+}
+
+export default PricingRulesForm
