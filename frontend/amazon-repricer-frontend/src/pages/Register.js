fe9ddb52f2af43a1230262b45035d15fe1f1561e
Yibin Long <yibinlong@outlook.com>
2024-09-21 14:05:29 -0700

    init register component

---
+++ frontend/amazon-repricer-frontend/src/pages/Register.js
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+
+const Register = () => {
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+};
+
+export default Register
