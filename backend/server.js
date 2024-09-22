cbce12edc3ce1257383e596bde3ba084ba320311
Yibin Long <yibinlong@outlook.com>
2024-09-22 15:24:39 -0700

    add auth to product and pricingrules

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ const protectedRoutes = require('./routes/protectedRoutes');
 const amazonRoutes = require('./routes/amazon');
 const productRoutes = require('./routes/products');
 const pricingRulesRoutes = require('./routes/pricingRules');
+const auth = require('./middleware/auth');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -16,8 +17,8 @@ app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api', protectedRoutes); 
 app.use('/api/amazon', amazonRoutes)
-app.use('/api/products', productRoutes);
-app.use('/api/pricing-rules', pricingRulesRoutes);
+app.use('/api/products', auth, productRoutes);
+app.use('/api/pricing-rules', auth, pricingRulesRoutes);
 
 app.get('/', (req, res) => {
   res.send('Amazon Repricer Backend');
