63dedeb32faa1c55bcc36d9b6d91eb8cd818e96b
Yibin Long <yibinlong@outlook.com>
2024-09-22 12:36:26 -0700

    add cors to server.js

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 // server.js
 const express = require('express');
+const cors = require('cors');
 
 const authRoutes = require('./routes/auth');
 const protectedRoutes = require('./routes/protectedRoutes');
@@ -10,6 +11,7 @@ const pricingRulesRoutes = require('./routes/pricingRules');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api', protectedRoutes); 
