d1cb4b8eaef8ae938581b327aa808753d7106095
Yibin Long <yibinlong@outlook.com>
2024-09-22 12:48:01 -0700

    fix up middleware authentication

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -4,10 +4,12 @@ const jwt = require('jsonwebtoken')
 const secret = process.env.JWT_SECRET;
 
 const auth = (req, res, next) => {
-    const token = req.header('Authorization').replace('Bearer ', '');
-    if (!token) {
-        return res.status(401).json({ message: 'No token, authorization denied'});
+    const authHeader = req.header('Authorization');
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+        return res.status(401).json({ message: 'No token, authorization denied' });
     }
+
+    const token = authHeader.replace('Bearer ', ''); // Extract token
 
     try {
         const decoded = jwt.verify(token, secret);
